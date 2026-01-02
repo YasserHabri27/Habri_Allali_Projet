e2f5663f1f3e9c941998aefd7e9c9c6aa9967840
YasserHabri27 <yasser.habri.dev2@gmail.com>
2026-01-02 04:47:50 +0100

    commit 4

---
+++ lib/features/projects/data/models/project_model.g.dart
@@ -0,0 +1,74 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'project_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ProjectModelAdapter extends TypeAdapter<ProjectModel> {
+  @override
+  final int typeId = 2;
+
+  @override
+  ProjectModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return ProjectModel(
+      id: fields[0] as String,
+      userId: fields[1] as String,
+      name: fields[2] as String,
+      description: fields[3] as String,
+      statusIndex: fields[4] as int,
+      progress: fields[5] as double,
+      startDate: fields[6] as DateTime,
+      endDate: fields[7] as DateTime,
+      createdAt: fields[8] as DateTime,
+      updatedAt: fields[9] as DateTime,
+      taskIds: (fields[10] as List?)?.cast<String>(),
+      colorHex: fields[11] as String?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, ProjectModel obj) {
+    writer
+      ..writeByte(12)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.userId)
+      ..writeByte(2)
+      ..write(obj.name)
+      ..writeByte(3)
+      ..write(obj.description)
+      ..writeByte(4)
+      ..write(obj.statusIndex)
+      ..writeByte(5)
+      ..write(obj.progress)
+      ..writeByte(6)
+      ..write(obj.startDate)
+      ..writeByte(7)
+      ..write(obj.endDate)
+      ..writeByte(8)
+      ..write(obj.createdAt)
+      ..writeByte(9)
+      ..write(obj.updatedAt)
+      ..writeByte(10)
+      ..write(obj.taskIds)
+      ..writeByte(11)
+      ..write(obj.colorHex);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ProjectModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}

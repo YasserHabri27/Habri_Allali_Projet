6f04511b033a7eae122e90b91577015184ac3fb8
YasserHabri27 <yasser.habri.dev2@gmail.com>
2026-01-02 03:49:36 +0100

    commit 2

--- lib/core/constants/api_constants.dart
+++
@@ -1,9 +0,0 @@
-class ApiConstants {
-  static const String baseUrl = 'https://api.pegasus-app.com';
-  static const String login = '/api/auth/login';
-  static const String register = '/api/auth/register';
-  static const String projects = '/api/projects';
-  static const String tasks = '/api/tasks';
-  static const Duration connectTimeout = Duration(seconds: 30);
-  static const Duration receiveTimeout = Duration(seconds: 30);
-}
